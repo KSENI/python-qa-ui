1cd874db802388afd52d99d6d4665115b2cbb0ef
Kseniia Pamberg <kse1non@gmail.com>
2021-12-02 20:39:20 +0300

    update test_guest_can_go_to_login_page

--- pages/locators.py
+++ pages/locators.py
@@ -5,8 +5,8 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_EMAIL = (By.CSS_SELECTOR, "input[name='login-username']")
     LOGIN_PASSWORD = (By.CSS_SELECTOR, "input[name='login-password']")
-    LOGIN_BUTTON = (By.CSS_SELECTOR, "input[name='login_submit']")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "button[name='login_submit']")
     REGISTRATION_EMAIL = (By.CSS_SELECTOR, "input[name='registration-email']")
     REGISTRATION_PASSWORD = (By.CSS_SELECTOR, "input[name='registration-password1']")
     REGISTRATION_PASSWORD_REPEAT = (By.CSS_SELECTOR, "input[name='registration-password2']")
-    REGISTRATION_BUTTON = (By.CSS_SELECTOR, "input[name='registration_submit']")
+    REGISTRATION_BUTTON = (By.CSS_SELECTOR, "button[name='registration_submit']")
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -8,15 +8,15 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in self.page_source
+        assert "login" in self.browser.page_source
 
     def should_be_login_form(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_EMAIL), "Login email is not presented"
-        assert self.is_element_present(*MainPageLocators.LOGIN_PASSWORD), "Login password is not presented"
-        assert self.is_element_present(*MainPageLocators.LOGIN_BUTTON), "Login button is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL), "Login email is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD), "Login password is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "Login button is not presented"
 
     def should_be_register_form(self):
-        assert self.is_element_present(*MainPageLocators.REGISTRATION_EMAIL), "Registration email is not presented"
-        assert self.is_element_present(*MainPageLocators.REGISTRATION_PASSWORD), "Registration password is not presented"
-        assert self.is_element_present(*MainPageLocators.REGISTRATION_PASSWORD_REPEAT), "Registration password repeat is not presented"
-        assert self.is_element_present(*MainPageLocators.REGISTRATION_BUTTON), "Registration button is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL), "Registration email is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD), "Registration password is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD_REPEAT), "Registration password repeat is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_BUTTON), "Registration button is not presented"
